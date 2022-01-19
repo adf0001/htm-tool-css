daa5f8b2ca22ac0b26d3c1b81f92deb5e32454b0
fwg <adf0001@163.com>
2022-01-19 11:15:47 +0800

    update: remove dependency ele-tool

--- htm-tool-css.js
+++ htm-tool-css.js
@@ -1,7 +1,6 @@
 ﻿
 // htm-tool-css @ npm, htm-tool css module.
 
-var ele = require("ele-tool");
 var add_css_text = require("add-css-text");
 
 // basic common css
@@ -13,13 +12,15 @@ add_css_text(require("./res/entity.css"), "ht-entity-css");
 var setSelected = function (selectList, unselectList, selected, selectedClass) {
 	if (!selected) { var tmp = selectList; selectList = unselectList; unselectList = tmp; }	//exchange
 
-	var i, imax;
+	var i, imax, li;
 	if (selectList) {
 		if (!(selectList instanceof Array)) selectList = [selectList];
 
 		imax = selectList.length;
 		for (i = 0; i < imax; i++) {
-			ele(selectList[i]).classList.add(selectedClass || "ht-selected");
+			li = selectList[i];
+			if (typeof li === "string") li = document.getElementById(li);
+			li.classList.add(selectedClass || "ht-selected");
 		}
 	}
 
@@ -28,7 +29,9 @@ var setSelected = function (selectList, unselectList, selected, selectedClass) {
 
 		imax = unselectList.length;
 		for (i = 0; i < imax; i++) {
-			ele(unselectList[i]).classList.remove(selectedClass || "ht-selected");
+			li = unselectList[i];
+			if (typeof li === "string") li = document.getElementById(li);
+			li.classList.remove(selectedClass || "ht-selected");
 		}
 	}
 }
